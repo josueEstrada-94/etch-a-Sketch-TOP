ec71bd296003842055d913139308b04898f09eb8
josueEstrada-94 <josue.estrada.94@gmail.com>
2023-11-02 20:44:25 -0600

    Inside the createContainer function was created the squared divs inside the container.

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
+document.addEventListener('DOMContentLoaded', function(){
+    createContainer(18);
+    console.log('hi');
+})
+
 //Creating the function that will return the grid container based on the size.
+
 function createContainer(size){
     let container = document.querySelector('#container');
 
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-}
-console.log('hi');
 
-createContainer(32);
+//Creating the squares for the divs inside the container dinamically.
+    let numDivs = size * size;
+
+    for (let i = 0; i < numDivs; i++){
+        let div = document.createElement('div');
+        div.style.backgroundColor = 'red';
+        container.insertAdjacentElement("beforeend", div);
+    }
+}
+
+
+
+ 
