719f6ccc747ee41d76e3b1f08436361b644f19b9
josueEstrada-94 <josue.estrada.94@gmail.com>
2023-11-02 21:20:57 -0600

    getSize function created. It asks the user for the size of the container, then generates the new container.

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
-    createContainer(18);
-    console.log('hi');
+    createContainer(16);
+
+    let popup = document.querySelector('#popup');
+    popup.addEventListener('click', function(){
+        let size = getSize();
+        createContainer(size);
+    })
 })
 
 //Creating the function that will return the grid container based on the size.
@@ -16,11 +21,26 @@ function createContainer(size){
 
     for (let i = 0; i < numDivs; i++){
         let div = document.createElement('div');
-        div.style.backgroundColor = 'red';
+        div.style.backgrounsdColor = 'red';
         container.insertAdjacentElement("beforeend", div);
     }
 }
 
+//Creating a function that ask the user for the size of the container.
 
+function getSize(){
+    let input = prompt('What will be the size of the board?')
+    let message = document.querySelector('#message');
+    if (input === ''){
+        message.innerHTML = 'Please provide a number';
+    }
+    else if(input < 0 || input > 100){
+        message.innerHTML = 'Please provide a number between 1 and 100'
+    }
+    else{
+        message.innerHTML = 'Now you can play!';
+        return input;
+    }
+}
 
  
